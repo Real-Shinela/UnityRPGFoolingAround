580a0397c50d9cac91b850fefbaf2ba4b5e3afb2
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-06-16 15:52:46 +0200

    First Work
    Base Error Handling and Preparations

---
+++ UnityRPGFoolingAround/Assets/Player_Master.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Master : MonoBehaviour
+{
+    [SerializeField] int MaxHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
