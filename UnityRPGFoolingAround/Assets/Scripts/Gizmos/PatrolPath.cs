c30ff7fee3c64225e42add80318436dfcb336106
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-07-29 14:57:06 +0200

    Gizmos

---
+++ UnityRPGFoolingAround/Assets/Scripts/Gizmos/PatrolPath.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Control
+{
+    public class PatrolPath : MonoBehaviour
+    {
+
+        private void OnDrawGizmos()
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Transform gizSpot = transform.GetChild(i);
+                Transform gizNextSpot =
+                    i == transform.childCount - 1 ?
+                    transform.GetChild(0) : transform.GetChild(i + 1);
+
+                Gizmos.color = Color.cyan;
+                Gizmos.DrawSphere(gizSpot.position, 0.4f);
+
+                Gizmos.DrawLine(gizSpot.position, gizNextSpot.position);
+            }
+        }
+    }
+}
