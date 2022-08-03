fc0f748f0ebf6075dd521ce6137df73703c45bb5
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-08-03 14:00:45 +0200

    why use many commit when one commit do

--- UnityRPGFoolingAround/Assets/Scripts/Gizmos/PatrolPath.cs
+++ UnityRPGFoolingAround/Assets/Scripts/Gizmos/PatrolPath.cs
@@ -11,17 +11,26 @@ namespace RPG.Control
         {
             for (int i = 0; i < transform.childCount; i++)
             {
-                Transform gizSpot = transform.GetChild(i);
                 /*Transform gizNextSpot =
                     i == transform.childCount - 1 ?
                     transform.GetChild(0) : transform.GetChild(i + 1);*/
-                Transform gizNextSpot = transform.GetChild((i + 1) % transform.childCount);
+                Transform gizNextSpot = transform.GetChild(GetNextIndex(i));
 
                 Gizmos.color = Color.cyan;
-                Gizmos.DrawSphere(gizSpot.position, 0.4f);
+                Gizmos.DrawSphere(GetWayPoint(i), 0.4f);
 
-                Gizmos.DrawLine(gizSpot.position, gizNextSpot.position);
+                Gizmos.DrawLine(GetWayPoint(i), gizNextSpot.position);
             }
+        }
+
+        public Vector3 GetWayPoint(int i)
+        {
+            return transform.GetChild(i).position;
+        }
+
+        public int GetNextIndex(int i)
+        {
+            return (i + 1) % transform.childCount;
         }
     }
 }
