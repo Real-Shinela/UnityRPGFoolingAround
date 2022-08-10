27ed985a5448d6aaa3e3422735f75b85caadff7e
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-08-10 13:04:09 +0200

    Scene 2 added for loadscene playing

---
+++ UnityRPGFoolingAround/Assets/Scripts/Scene Management/Portal.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene("Sandbox", LoadSceneMode.Single);
+        }
+    }
+}
