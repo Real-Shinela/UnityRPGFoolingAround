fa9cda7080f3ff3d5e441aa5eca8f83203d3eb05
Shinela <105044657+Real-Shinela@users.noreply.github.com>
2022-08-10 15:28:28 +0200

    portalising

--- UnityRPGFoolingAround/Assets/Scripts/Scene Management/Portal.cs
+++ UnityRPGFoolingAround/Assets/Scripts/Scene Management/Portal.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
     [SerializeField] int sceneToLoad = -1;
-
+    [SerializeField] Transform spawnPoint;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,7 +21,27 @@ public class Portal : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
         yield return SceneManager.LoadSceneAsync(sceneToLoad);
-        print("Scene Loaded");
+
+        Portal otherPortal = GetOtherPortal();
+        UpdatePlayer(otherPortal);
+
         Destroy(gameObject);
+    }
+
+    private void UpdatePlayer(Portal otherPortal)
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        player.GetComponent<NavMeshAgent>().Warp(otherPortal.spawnPoint.position);
+    }
+
+    private Portal GetOtherPortal()
+    {
+        foreach (Portal portal in FindObjectsOfType<Portal>())
+        {
+            if (portal == this) continue;
+            return portal;
+        }
+
+        return null;
     }
 }
